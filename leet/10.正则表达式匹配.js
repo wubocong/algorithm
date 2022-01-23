c373dbcc2d0cb09dc8badc922471e28cf6600afd
wubocong <wubocong123@163.com>
2022-01-23 19:00:04 +0800

    edit-solution：10

--- leet/10.正则表达式匹配.js
+++ leet/10.正则表达式匹配.js
@@ -37,7 +37,7 @@
 //   return dp(0, 0);
 // };
 
-// dp table解法
+// dp table解法，可以进一步使用状态压缩
 var isMatch = function (s, p) {
   const sLength = s.length, pLength = p.length;
   const dp = [];
@@ -49,8 +49,7 @@ var isMatch = function (s, p) {
   }
   dp[sLength][pLength] = true;
   for (let si = sLength; si >= 0; si--) {
-    for (let pi = pLength; pi >= 0; pi--) {
-      if (si === sLength && pi === pLength) continue;
+    for (let pi = pLength - 1; pi >= 0; pi--) { // si===sLength，这一行可能为true，因为p剩余的可以不匹配，但pi===pLength这一行一定为false，因为模式串一镜到底，而待匹配的串还没完全匹配
       if (s[si] === p[pi] || p[pi] === '.') {
         if (p[pi + 1] === '*') dp[si][pi] = (pi + 2 <= pLength && dp[si][pi + 2]) || (si + 1 <= sLength && dp[si + 1][pi])
         else dp[si][pi] = (si + 1 <= sLength && pi + 1 <= pLength && dp[si + 1][pi + 1])
