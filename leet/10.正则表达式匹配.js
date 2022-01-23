b285551bcf765de26e90bb5bb61fefa7f15ddc9a
wubocong <wubocong123@163.com>
2022-01-23 18:55:03 +0800

    edit-solution: 10

--- leet/10.正则表达式匹配.js
+++ leet/10.正则表达式匹配.js
@@ -10,29 +10,56 @@
  * @param {string} p
  * @return {boolean}
  */
+// 递归dp解法
+// var isMatch = function (s, p) {
+//   const memo = {}
+//   const sLength = s.length, pLength = p.length;
+//   function dp(si, pi) {
+//     if (si === sLength && pi === pLength) return true;
+//     if (si > sLength || pi > pLength) return false;
+//     const key = si + ',' + pi;
+//     if (memo[key] !== undefined) return memo[key];
+//     if (s[si] === p[pi] || p[pi] === '.') {
+//       if (p[pi + 1] === '*') {
+//         memo[key] = dp(si, pi + 2) || dp(si + 1, pi);
+//       } else {
+//         memo[key] = dp(si + 1, pi + 1)
+//       }
+//     } else {
+//       if (p[pi + 1] === '*') {
+//         memo[key] = dp(si, pi + 2);
+//       } else {
+//         memo[key] = false;
+//       }
+//     }
+//     return memo[key];
+//   }
+//   return dp(0, 0);
+// };
+
+// dp table解法
 var isMatch = function (s, p) {
-  const memo = {}
   const sLength = s.length, pLength = p.length;
-  function dp(si, pi) {
-    if (si === sLength && pi === pLength) return true;
-    if (si > sLength || pi > pLength) return false;
-    const key = si + ',' + pi;
-    if (memo[key] !== undefined) return memo[key];
-    if (s[si] === p[pi] || p[pi] === '.') {
-      if (p[pi + 1] === '*') {
-        memo[key] = dp(si, pi + 2) || dp(si + 1, pi) || dp(si + 1, pi + 2);
+  const dp = [];
+  for (let i = 0; i <= sLength; i++) {
+    dp.push([]);
+    for (let j = 0; j <= pLength; j++) {
+      dp[i][j] = false;
+    }
+  }
+  dp[sLength][pLength] = true;
+  for (let si = sLength; si >= 0; si--) {
+    for (let pi = pLength; pi >= 0; pi--) {
+      if (si === sLength && pi === pLength) continue;
+      if (s[si] === p[pi] || p[pi] === '.') {
+        if (p[pi + 1] === '*') dp[si][pi] = (pi + 2 <= pLength && dp[si][pi + 2]) || (si + 1 <= sLength && dp[si + 1][pi])
+        else dp[si][pi] = (si + 1 <= sLength && pi + 1 <= pLength && dp[si + 1][pi + 1])
       } else {
-        memo[key] = dp(si + 1, pi + 1)
-      }
-    } else {
-      if (p[pi + 1] === '*') {
-        memo[key] = dp(si, pi + 2);
-      } else {
-        memo[key] = false;
+        if (p[pi + 1] === '*') dp[si][pi] = (pi + 2 <= pLength && dp[si][pi + 2]);
+        else dp[si][pi] = false;
       }
     }
-    return memo[key];
   }
-  return dp(0, 0);
-};
+  return dp[0][0];
+}
 // @lc code=end
